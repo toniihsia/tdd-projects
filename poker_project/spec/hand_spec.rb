c96986f14d7702fc4c2275a2b6f44dd0ffca3073
App Academy Student <student@appacademy.io>
2016-09-21 17:35:21 -0700

    add expects to spec for Hand

--- poker_project/spec/hand_spec.rb
+++ poker_project/spec/hand_spec.rb
@@ -11,76 +11,55 @@ describe Hand do
     end
   end
 
-  describe "#is_flush?" do
-    it "returns true if it is a flush" do
-      let(:card_one) {double("card_one", :suit => :H, :value => 2)}
-      let(:card_two) {double("card_two", :suit => :H, :value => 3)}
-      let(:card_three) {double("card_three", :suit => :H, :value => 6)}
+  describe "handscore" do
+    it "returns royal flush value as 10" do
+      let(:card_one) {double("card_one", :suit => :H, :value => 14)}
+      let(:card_two) {double("card_two", :suit => :H, :value => 13)}
+      let(:card_three) {double("card_three", :suit => :H, :value => 12)}
+      let(:card_four) {double("card_four", :suit => :H, :value => 11)}
+      let(:card_five) {double("card_five", :suit => :H, :value => 10)}
+      hand.cards = [card_one, card_two, card_three, card_four, card_five]
+      expect(hand.handscore).to eq(10)
+    end
+
+    it "returns straight flush value as 9" do
+      let(:card_one) {double("card_one", :suit => :H, :value => 10)}
+      let(:card_two) {double("card_two", :suit => :H, :value => 9)}
+      let(:card_three) {double("card_three", :suit => :H, :value => 8)}
       let(:card_four) {double("card_four", :suit => :H, :value => 7)}
-      let(:card_five) {double("card_five", :suit => :H, :value => 9)}
-      hand.cards = [card_one, card_two, card_three, card_four, card_five]
-      expect(hand.is_flush?)to be_true
-    end
-  end
-
-  describe "#is_straight?" do
-    it "returns true if it is a straight" do
-      let(:card_one) {double("card_one", :suit => :H, :value => 2)}
-      let(:card_two) {double("card_two", :suit => :C, :value => 3)}
-      let(:card_three) {double("card_three", :suit => :H, :value => 4)}
-      let(:card_four) {double("card_four", :suit => :D, :value => 5)}
-      let(:card_five) {double("card_five", :suit => :S, :value => 6)}
-      hand.cards = [card_one, card_two, card_three, card_four, card_five]
-      expect(hand.is_straight?)to be_true
-    end
-  end
-
-  describe "#is_straightflush?" do
-    it "returns true if it is a flush" do
-      let(:card_one) {double("card_one", :suit => :H, :value => 2)}
-      let(:card_two) {double("card_two", :suit => :H, :value => 3)}
-      let(:card_three) {double("card_three", :suit => :H, :value => 4)}
-      let(:card_four) {double("card_four", :suit => :H, :value => 5)}
       let(:card_five) {double("card_five", :suit => :H, :value => 6)}
       hand.cards = [card_one, card_two, card_three, card_four, card_five]
-      expect(hand.is_straightflush?)to be_true
+      expect(hand.handscore).to eq(9)
+    end
+
+    it "returns four of a kind value as 8" do
+      let(:card_one) {double("card_one", :suit => :S, :value => 3)}
+      let(:card_two) {double("card_two", :suit => :H, :value => 3)}
+      let(:card_three) {double("card_three", :suit => :D, :value => 3)}
+      let(:card_four) {double("card_four", :suit => :C, :value => 3)}
+      let(:card_five) {double("card_five", :suit => :H, :value => 10)}
+      hand.cards = [card_one, card_two, card_three, card_four, card_five]
+      expect(hand.handscore).to eq(8)
+    end
+
+    it "returns full house value as 7" do
+      let(:card_one) {double("card_one", :suit => :H, :value => 10)}
+      let(:card_two) {double("card_two", :suit => :S, :value => 10)}
+      let(:card_three) {double("card_three", :suit => :D, :value => 10)}
+      let(:card_four) {double("card_four", :suit => :S, :value => 9)}
+      let(:card_five) {double("card_five", :suit => :H, :value => 9)}
+      hand.cards = [card_one, card_two, card_three, card_four, card_five]
+      expect(hand.handscore).to eq(7)
+    end
+
+    it "returns two pairs as 3" do
+      let(:card_one) {double("card_one", :suit => :H, :value => 2)}
+      let(:card_two) {double("card_two", :suit => :S, :value => 2)}
+      let(:card_three) {double("card_three", :suit => :D, :value => 3)}
+      let(:card_four) {double("card_four", :suit => :S, :value => 3)}
+      let(:card_five) {double("card_five", :suit => :H, :value => 9)}
+      hand.cards = [card_one, card_two, card_three, card_four, card_five]
+      expect(hand.handscore).to eq(3)
     end
   end
-
-  describe "#handscore" do
-    
-    it "returns true if it is a flush" do
-      let(:card_one) {double("card_one", :suit => :H, :value => 2)}
-      let(:card_two) {double("card_two", :suit => :C, :value => 2)}
-      let(:card_three) {double("card_three", :suit => :C, :value => 3)}
-      let(:card_four) {double("card_four", :suit => :S, :value => 3)}
-      let(:card_five) {double("card_five", :suit => :D, :value => 3)}
-      hand.cards = [card_one, card_two, card_three, card_four, card_five]
-      expect(hand.handscore)to eq(7)
-    end
-
-    it "returns true if it is a flush" do
-      let(:card_one) {double("card_one", :suit => :H, :value => 2)}
-      let(:card_two) {double("card_two", :suit => :C, :value => 2)}
-      let(:card_three) {double("card_three", :suit => :C, :value => 3)}
-      let(:card_four) {double("card_four", :suit => :S, :value => 3)}
-      let(:card_five) {double("card_five", :suit => :D, :value => 3)}
-      hand.cards = [card_one, card_two, card_three, card_four, card_five]
-      expect(hand.handscore)to eq(7)
-    end
-
-    it "returns true if it is a flush" do
-      let(:card_one) {double("card_one", :suit => :H, :value => 2)}
-      let(:card_two) {double("card_two", :suit => :C, :value => 2)}
-      let(:card_three) {double("card_three", :suit => :C, :value => 3)}
-      let(:card_four) {double("card_four", :suit => :S, :value => 3)}
-      let(:card_five) {double("card_five", :suit => :D, :value => 4)}
-      hand.cards = [card_one, card_two, card_three, card_four, card_five]
-      expect(hand.handscore)to eq(3)
-    end
-
-  end
-
-
-
 end
